759a23456a34afcfad7d57171cdd833b8ac85380
hanul <80873494+iamhanori@users.noreply.github.com>
2022-04-01 01:14:28 +0900

    0331 phone number (#10)
    * [ADD] 0331 phone_number
    
    * [UPDATE] 0331 phone_number
    
    * [UPDATE] 0331 phone_number

---
+++ src/programmers/phone_number.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class phone_number {
+
+    public String solution(String phone_number) {
+        String answer = "";
+
+        for(int i = 0; i < phone_number.length(); i++){
+            if(i < phone_number.length()-4){
+                answer += "*";
+            }
+            else {
+                answer += phone_number.charAt(i);
+            }
+        }
+        return answer;
+    }
+
+}
