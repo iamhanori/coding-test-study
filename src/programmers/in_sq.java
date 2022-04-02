b1b64deecd42c20c664ecc8d8af62162384ae763
hanul <80873494+iamhanori@users.noreply.github.com>
2022-04-02 11:38:44 +0900

    [ADD] 0401 in_sq (#12)
    Co-authored-by: 김하늘 <80873494+hanul1206@users.noreply.github.com>

---
+++ src/programmers/in_sq.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.Scanner;
+public class in_sq {
+    public static long solution(long n) {
+        long answer = 0;
+        answer = (long) Math.sqrt(n);
+        if (n == (long) Math.pow(answer, 2)) {
+            answer = (long) Math.pow(answer + 1, 2);
+        } else
+            answer = -1;
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        System.out.println(solution(n));
+
+    }
+}
