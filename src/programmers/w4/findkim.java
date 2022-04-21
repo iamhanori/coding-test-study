db8e372557efc16e4ff000c46d4accf1f2cc2eaa
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-04-21 18:30:11 +0900

    [ADD] 0421 findkim

--- src/programmers/w4/findkim.java
+++ src/programmers/w4/findkim.java
@@ -4,8 +4,8 @@ public class findkim {
         public String solution(String[] seoul) {
             String answer = "";
 
-            for(int i = 0; i < seoul.length; i++) {
-                if(seoul[i].equals("Kim")) {
+            for (int i = 0; i < seoul.length; i++) {
+                if (seoul[i].equals("Kim")) {
                     answer = "김서방은 " + i + "에 있다";
                     break;
                 }
